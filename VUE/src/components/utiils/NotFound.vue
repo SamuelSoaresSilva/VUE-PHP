2aa8ed44151287644b35ed7491f38259f3cafdd5
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-04-06 20:30:09 -0300

    Complete not found page

--- VUE/src/components/utiils/NotFound.vue
+++ VUE/src/components/utiils/NotFound.vue
@@ -13,17 +13,23 @@ onMounted(() => addLetter(message))
 </script>
 
 <template>
-  <section class="grid text-center">
-    <div class="row justify-content-md-center align-items-center">
-      <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGU0OXFoYjVtOW15em5vNXFsajFoNTczMXl4ZDVidGRldGQybTdoaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l4pSXkg0Ss5mlxc8o/giphy.gif"
-           class="img-fluid col-md-auto"
-      />
-      <h1 class="roboto-bold col-md-auto">
-        [ {{ msg }} ].
-      </h1>
-    </div>
+  <section class="background-gif ">
+      <div class="blur-container d-flex entire-page-container align-items-center justify-content-center text-light">
+        <router-link class="roboto-bold fs-1 header-title"
+        :to="{ name: 'home' }"
+        >
+          [ {{ msg }} ].
+        </router-link>
+      </div>
   </section>
 </template>
 <style scoped>
-
+.background-gif{
+  background-image: url("@/assets/images/backgroundGif.gif");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.blur-container{
+  backdrop-filter: blur(3rem);
+}
 </style>
