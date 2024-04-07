279dd63ca2dfd9170742dff38e9b772d4badebd6
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-04-06 22:10:15 -0300

    Coponentize Title button

---
+++ VUE/src/components/AppTitleButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const props = defineProps(['title'])
+</script>
+
+<template>
+  <router-link
+    :to="{ name: 'home' }"
+    class="fs-1 header-title"
+  >
+    [ {{ props.title }} ].
+  </router-link>
+</template>
+
+<style scoped>
+
+</style>
