4f27576e9f1b834c2bfbac2a4777f081f44386c6
Cauã Prates <caualucagp76@gmail.com>
2024-04-10 03:26:20 -0300

    Creating new form to update users

--- VUE/src/views/user/RegisterLoginView.vue
+++ VUE/src/views/user/RegisterLoginView.vue
@@ -5,6 +5,7 @@ import { useAddLetter } from '@/composables/useAddLetter.js'
 import UserRegisterForm from '@/components/user/UserRegisterForm.vue'
 import AppTitleButton from '@/components/AppTitleButton.vue'
 import UserLoginForm from '@/components/user/UserLoginForm.vue'
+import UserUpdateForm from '@/components/user/UserUpdateForm.vue'
 
 const havesAccount = ref(false);
 const loginMsg = ref("Already have an account? \nLog in by clicking below.")
@@ -42,7 +43,8 @@ watch(havesAccount,() => {
       </section>
       <section class="col bg-dark bg-opacity-50 rounded-4 p-4 align-content-center">
         <UserRegisterForm v-if="!havesAccount"/>
-        <UserLoginForm v-else/>
+        <!-- <UserLoginForm v-else/> -->
+        <UserUpdateForm v-else/>
       </section>
     </section>
     <br>
